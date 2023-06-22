ed59570fdf96b69696c2cbcfdacc3d3ab2c229e7
Diana Asmus <asmus.diana@icloud.com>
2023-06-22 18:46:55 +0200

    move Character

--- models/character.class.js
+++ models/character.class.js
@@ -1,5 +1,6 @@
 class Character extends MovableObject {
     world; // character hat eine Variable namens 'world', womit wir auf die variablen aus der world zugreifen können => keyboard
+    speed = 7;
 
     IMAGES_WALKING = [ //Übersichtlicher
         'img/alien/walk1.png',
@@ -18,8 +19,18 @@ class Character extends MovableObject {
     }
 
     animate() {
-        setInterval(() => { //jedes bild wird 1 sekunde angezeigt, dann currentImage++
+
+        setInterval(() => {
             if (this.world.keyboard.RIGHT) {
+                this.x += this.speed;
+            }
+            if (this.world.keyboard.LEFT) {
+                this.x -= this.speed;
+            }
+        }, 1000 / 60);
+
+        setInterval(() => { //jedes bild wird 1 sekunde angezeigt, dann currentImage++
+            if (this.world.keyboard.RIGHT || this.world.keyboard.LEFT) { //Animation wird abgespielt, wenn keyboard gedrückt
                 let i = this.currentImage % this.IMAGES_WALKING.length; //Modulo: let i = 0 % 6; => Stelle[0] 0, rest 0 ... Stelle [1] 0, rest 1 ... 
                 // Stelle [7] = 1, rest 1 => nur 1 wird aufgerufen!! 
                 let path = this.IMAGES_WALKING[i];
